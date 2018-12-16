7d462526ee9a52d2124cb8b76a2b95b6f85e41d5
PAPICO <ocipap0531@gmail.com>
2018-12-16 23:33:33 +0900

    feat: 초반 프로젝트 셋팅

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <router-link></router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+html, body {
+  width: 100%;
+  height: 100%;
+}
+
+#app {
+  width: 100%;
+  height: 100%;
+}
+
+.wrapper{
+  width: 1080px;
+  margin: 0 auto;
+}
+</style>
