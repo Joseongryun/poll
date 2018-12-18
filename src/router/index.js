06e738b750ee21041848626dd4a96d408cd862cb
PAPICO <ocipap0531@gmail.com>
2018-12-18 23:49:14 +0900

    feat: 기본 라우팅 및 페이지 추가

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,63 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Home from '@/views/Home'
+import Login from '@/views/Login'
+import Join from '@/views/Join'
+import PollList from '@/views/PollList'
+import Poll from '@/views/Poll'
+import PollInput from '@/views/PollInput'
+import PollResult from "@/view/PollResult"
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
+  mode: 'history',
+  routes: [{
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/join',
+      name: 'Join',
+      component: Join
+    }, 
+    {
+      path: '/polls/:menu',
+      name: 'PollList',
+      component: PollList,
+      meta : {
+        requiresAuth: true
+      }
+    },
+    {
+      path: '/polls/detail/:pid',
+      name: 'Poll',
+      component: Poll,
+      meta: {
+        requiresAuth: true
+      }
+    },
+    {
+      path: '/polls/post',
+      name: 'PollInput',
+      component: PollInput,
+      meta: {
+        requiresAuth: true
+      }
+    },
+    {
+      path: '/polls/result/:pid',
+      name: 'PollResult',
+      component: PollResult,
+      meta: {
+        requiresAuth: true
+      }
     }
   ]
 })
